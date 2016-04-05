bf1b68a18d33d5c87c6abcd95607de28f26075f3
Josh Medeski <josh.medeski@gmail.com>
2016-04-05 15:32:03 -0500

    Setup example page (for testing)

---
+++ example/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp  = require('gulp');
+var $     = require('gulp-load-plugins')();
+var BOWER = 'bower_components';
+
+var sassPaths = [
+  BOWER + '/foundation-sites/scss',
+  BOWER + '/foundation-font-awesome-buttons/scss',
+  BOWER + '/fontawesome/scss',
+];
+
+gulp.task('sass', function () {
+  return gulp.src('scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9'],
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['sass'], function () {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+});
