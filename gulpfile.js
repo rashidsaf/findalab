91a5fba4c3547a76186d91d0564863e7cd245957
crschoen <crschoen@gmail.com>
2016-04-28 13:39:41 -0500

    Removed Foundation & Updated Bower Script

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,12 @@ var autoprefixerBrowsers = {
   browsers: ['last 2 versions', 'ie >= 9'],
 };
 
-var sassPaths = [
-  BOWER + '/foundation-sites/scss',
-  BOWER + '/foundation-font-awesome-buttons/scss',
-  BOWER + '/fontawesome/scss',
-  'src',
-];
-
-gulp.task('sass', function() {
-  return gulp.src('scss/*.scss')
-  .pipe(sass({
-    includePaths: sassPaths,
-  })
-  .on('error', sass.logError))
-  .pipe(autoprefixer(autoprefixerBrowsers))
+gulp.task('stylesheet', function() {
+  return gulp.src('src/findalab.scss')
+  .pipe(sass(autoprefixerBrowsers))
   .pipe(gulp.dest('css'));
 });
 
-gulp.task('stylesheet', function() {
-  return gulp.src('css/findalab.scss')
-  .pipe(sass(autoprefixerBrowsers))
-  .pipe(gulp.dest(''));
-});
-
-gulp.task('default', ['sass', 'stylesheet'], function() {
-  gulp.watch(['scss/*.scss'], ['sass', 'stylesheet']);
+gulp.task('default', ['stylesheet'], function() {
   gulp.watch(['src/*.scss'], ['sass', 'stylesheet']);
 });
