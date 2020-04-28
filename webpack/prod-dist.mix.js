9073d598c03c22d6fc1c8e6791124e26b68d99c7
Rashid <52429111+rashidsaf@users.noreply.github.com>
2020-04-28 10:36:53 -0500

    Minified and regular dist files (#189)

--- webpack/prod-dist.mix.js
+++ webpack/prod-dist.mix.js
@@ -1,10 +1,10 @@
 require('laravel-mix')
-  .js('src/js/findalab.js', 'dist')
+  .js('src/js/findalab.js', 'dist/findalab.min.js')
   .sass('src/scss/findalab.scss', 'dist')
   .copy('dist/findalab.css', 'dev/html/css/findalab.css')
+  .copy('src/js/findalab.js', 'dist')
   .webpackConfig({
     output: {
-      library: '',
-      libraryTarget: 'commonjs-module'
-      }
+      library: 'findALab'
+    }
   });
